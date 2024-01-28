f96b4fdc7d93396e6689eb6595775296166f2fe1
tschmidty69 <tschmidty@yahoo.com>
2024-01-28 16:02:30 -0500

    Sync

--- examples/tschmidt/main.tf
+++ examples/tschmidt/main.tf
@@ -4,6 +4,8 @@ provider "aws" {
 
 data "aws_organizations_organization" "org" {}
 locals {
-  member_account_ids = data.aws_organizations_organization.org.non_master_accounts[*].id
-  all_account_ids    = data.aws_organizations_organization.org.accounts[*].id
+  member_account_ids =  [ for account in data.aws_organizations_organization.org.non_master_accounts: account.id
+                            if account.status == "ACTIVE" ]
+  all_account_ids    =  [ for account in data.aws_organizations_organization.org.accounts : account.id 
+                          if account.status == "ACTIVE" ]
 }
